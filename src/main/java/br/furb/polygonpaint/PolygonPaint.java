980c69523de2706951c9dc315dacbdcf5d9c7e59
matheushoeltgebaum <pereiramateduardo@gmail.com>
2017-09-12 20:23:57 +0000

    Ajustes iniciais

--- src/main/java/br/furb/polygonpaint/PolygonPaint.java
+++ src/main/java/br/furb/polygonpaint/PolygonPaint.java
@@ -2,6 +2,6 @@ package br.furb.polygonpaint;
 
 public class PolygonPaint {
     public static void main(String[] args) {
-
+        new World().setVisible(true);
     }
 }
--- src/main/java/br/furb/polygonpaint/World.java
+++ src/main/java/br/furb/polygonpaint/World.java
@@ -1,13 +1,34 @@
 package br.furb.polygonpaint;
 
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLCapabilities;
+import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
-public class World {
+public class World extends JFrame {
 
     private Camera camera;
     private GraphicalObject selectedGraphicalObject;
     private Color backgroundColor;
     private List<GraphicalObject> graphicalObjects;
+
+    public World() {
+        setTitle("Polygon Paint");
+        setBounds(300, 250, 400, 400 + 22);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new BorderLayout());
+
+        GLCapabilities glCaps = new GLCapabilities();
+        glCaps.setRedBits(8);
+        glCaps.setBlueBits(8);
+        glCaps.setGreenBits(8);
+        glCaps.setAlphaBits(8);
+
+        GLCanvas canvas = new GLCanvas(glCaps);
+        add(canvas, BorderLayout.CENTER);
+        canvas.requestFocus();
+    }
 
     public Camera getCamera() {
         return camera;
